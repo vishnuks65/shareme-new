f92b0eb3631e968a2a8802cf976b348094bcb74a
mmuralidharan <mmuralidharan@opentrends.net>
2024-02-05 23:09:09 +0530

    front-end changes

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate  } from 'react-router-dom';
+
+
+const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
